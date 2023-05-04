32f7db0095388b29353d6fde0cdc134f90b8215a
Melanie Wong <wong.mela@gmail.com>
2023-05-03 20:47:33 -0700

    add styles and components

---
+++ pages/components/HamburgerMenu.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function HamburgerMenu () {
+    return <div>Menu</div>
+}
---
+++ pages/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "./Logo";
+import HamburgerMenu from "./HamburgerMenu";
+
+export default function Header() {
+  return (
+    <div>
+      <Logo />
+      <HamburgerMenu />
+    </div>
+  );
+}
