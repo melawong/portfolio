fe8d7d7eb853ae0fb2a8c9f319689daf34be3d8b
Melanie Wong <wong.mela@gmail.com>
2023-09-04 22:09:41 -0700

    update styling / homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,41 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import Header from "../components/Header";
-import Canvas from "@/components/Canvas";
-
-const inter = Inter({ subsets: ["latin"] });
+import { useRouter } from "next/router";
+import React from "react";
+// import Canvas from "@/components/Canvas";
 
 export default function Home() {
-  const draw = (ctx, frameCount) => {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    ctx.fillStyle = "#000000";
-    ctx.beginPath();
-    ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
-    ctx.fill();
-  };
+  // const draw = (ctx, frameCount) => {
+  //   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  //   ctx.fillStyle = "#000000";
+  //   ctx.beginPath();
+  //   ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
+  //   ctx.fill();
+  // };
+  const router = useRouter();
   return (
     <main className="top-level">
-      <div className="content__container--double">
-        <Header />
+      <div className="homebox">
+        <h1 className="title--homepage shimmer-bounce">
+          <span>W</span>
+          <span>E</span>
+          <span>L</span>
+          <span>C</span>
+          <span>O</span>
+          <span>M</span>
+          <span>E</span>
+          <span>!</span>
+        </h1>
+        <div className="home__container--buttons">
+          <button type="button" onClick={() => router.push("/about")}>
+            About
+          </button>
+          <button type="button" onClick={() => router.push("/portfolio")}>
+            Portfolio
+          </button>
+          <button type="button" onClick={() => router.push("/contact")}>
+            Contact
+          </button>
+        </div>
       </div>
-      <Canvas draw={draw} />
     </main>
   );
 }
